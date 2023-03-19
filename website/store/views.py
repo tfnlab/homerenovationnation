7145bb517ea2640a88835a7e2246fb760770b36c
TFNLab <tfnlab@gmail.com>
2023-03-19 13:51:47 -0700

    update

--- website/store/views.py
+++ website/store/views.py
@@ -593,7 +593,11 @@ def calculate_price(request):
 
 def access_backend_request(request, url):
     username = request.user.username
-    url = f'https://homerenovationnation.com/{url}?username={username}'
+    if '?' in url:
+        url = f'https://homerenovationnation.com/{url}&username={username}'
+    else:
+        url = f'https://homerenovationnation.com/{url}?username={username}'
+
     if request.method == 'POST':
         print("POST method called")
         data = {

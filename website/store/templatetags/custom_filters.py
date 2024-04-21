2fa791e36fe6132bb2a4c5a16fd0b6edf42609a8
Armen Merikyan <armenmerikyan@ArmensMacStudio.lan>
2024-04-21 00:45:50 -0700

    update with json

--- website/store/templatetags/custom_filters.py
+++ website/store/templatetags/custom_filters.py
@@ -42,3 +42,34 @@ def get_wallet_balance(wallet_address):
         print("Error:", e)
     
     # Convert lamports to SOL
+
+@register.filter
+def get_wallet_token_balance(wallet_address, token_address):
+# Define RPC server details
+    rpc_url = "https://weathered-long-wind.solana-mainnet.quiknode.pro/9c2a29380646c0e36d464da8c7424a981c931107/"  # Replace with your desired Solana RPC URL
+
+    # Token address and wallet address for which you want to check the balance
+    # JSON-RPC request payload to get token balance
+    payload = {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "getTokenAccountBalance",
+        "params": [
+            wallet_address,
+            {
+                "encoding": "jsonParsed",
+                "mint": token_address
+            }
+        ]
+    }
+
+    # Make the request
+    try:
+        response = requests.post(rpc_url, json=payload)
+        response.raise_for_status()  # Raise an error for bad responses
+        data = response.json()
+        balance = data["result"]["value"]["uiAmount"]
+        print("Token Balance:", balance)
+        return balance
+    except requests.exceptions.RequestException as e:
+        print("Error:", e)    

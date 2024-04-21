997ed83b8f0d5b972c22587984bf40cce3767c6e
Armen Merikyan <armenmerikyan@ArmensMacStudio.lan>
2024-04-21 00:37:45 -0700

    update with json

--- website/store/templatetags/custom_filters.py
+++ website/store/templatetags/custom_filters.py
@@ -1,7 +1,6 @@
 # custom_filters.py
+import requests
 from django import template
-from solana.account import Account
-from solana.rpc.api import Client
 
 register = template.Library()
 
@@ -16,20 +15,30 @@ def round_to_integer(value):
 @register.filter
 def get_wallet_balance(wallet_address):
     # Initialize a Solana RPC client
-    rpc_client = Client("https://api.mainnet-beta.solana.com")
+    # Define RPC server details
+    rpc_url = "https://weathered-long-wind.solana-mainnet.quiknode.pro/9c2a29380646c0e36d464da8c7424a981c931107/"  # Replace with your desired Solana RPC URL
 
-    # Get account information
-    account_info = rpc_client.get_account_info(wallet_address)
-    
-    # Check if the account exists
-    if account_info is None:
-        print("Account not found.")
-        return None
-    
-    # Decode the account data to extract the balance
-    balance = account_info["result"]["value"]["lamports"]
+    # Wallet address for which you want to check the balance
+    wallet_address = wallet_address
+
+    # JSON-RPC request payload to get balance
+    payload = {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "getBalance",
+        "params": [wallet_address]
+    }
+
+    # Make the request
+    try:
+        response = requests.post(rpc_url, json=payload)
+        response.raise_for_status()  # Raise an error for bad responses
+        data = response.json()
+        balance = data["result"]["value"]
+        print("Balance:", balance)
+        sol_balance = balance / 10**9        
+        return sol_balance   
+    except requests.exceptions.RequestException as e:
+        print("Error:", e)
     
     # Convert lamports to SOL
-    sol_balance = balance / 10**9
-    
-    return sol_balance   

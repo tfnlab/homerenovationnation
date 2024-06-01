ee042096e0bfece8cb248361e1065f4b312f4c77
Best Damn Cat <bestdamncat@gmail.com>
2024-06-01 13:46:12 -0700

    update

---
+++ website/website/celery.py
@@ -0,0 +1,23 @@
+# myproject/celery.py
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+from celery.schedules import crontab
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+
+app = Celery('myproject')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    'add-every-30-seconds': {
+        'task': 'myapp.tasks.add',
+        'schedule': 30.0,
+        'args': (16, 16)
+    },
+}
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')

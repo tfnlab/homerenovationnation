9a56f8360130334df973a0238d19512de920d2c3
Best Damn Cat <bestdamncat@gmail.com>
2024-06-01 13:59:56 -0700

    update

--- website/website/celery.py
+++ website/website/celery.py
@@ -6,7 +6,7 @@ from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'website.settings')
 
-app = Celery('myproject')
+app = Celery('website')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 

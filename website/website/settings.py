81e29b69c59c879dc4e8de7cbd06c4b0c11f3fe0
Best Damn Cat <bestdamncat@gmail.com>
2024-06-20 16:41:05 -0700

    update

--- website/website/settings.py
+++ website/website/settings.py
@@ -252,3 +252,5 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'

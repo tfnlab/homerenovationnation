e94828441555af2cf6f4e8eb0b29870b2969804f
Best Damn Cat <bestdamncat@gmail.com>
2024-06-20 15:49:55 -0700

    update

--- website/website/urls.py
+++ website/website/urls.py
@@ -72,7 +72,7 @@ urlpatterns = [
     
     path('create_token/', views.create_token, name='create_token'),
     path('get_count/', views.get_count, name='get_count'),
-    path('bundlecheckerview/', view.bundlecheckerview, name='bundlecheckerview'),
+    path('bundlecheckerview/', views.bundlecheckerview, name='bundlecheckerview'),
 
 
     path('admin/', admin.site.urls),

caca3996c150c3146a9d730f0a51a4642e99513d
Best Damn Cat <bestdamncat@gmail.com>
2024-07-10 18:44:17 -0700

    update

--- website/store/views.py
+++ website/store/views.py
@@ -97,22 +97,31 @@ import base58
 from nacl.signing import VerifyKey
 from nacl.exceptions import BadSignatureError
 
- 
+@csrf_exempt 
 def toggle_scam_filter(request):
-    access_id = request.COOKIES.get('access_id')
-    
-    if not access_id:
-        return JsonResponse({'error': 'Access ID not found in cookies.'}, status=400)
+    if request.method == 'POST':
+        # Get the access cookie and hidden field value from the request
+        access_cookie = request.COOKIES.get('access_cookie')
+        hidden_field_value = request.POST.get('hiddenField')
+        
+        # Check if the access cookie matches in the Accesstoken model
+        access_token = get_object_or_404(Accesstoken, access_cookie=access_cookie)
 
-    access_token = get_object_or_404(Accesstoken, access_cookie=access_id)
-    # Toggle the is_scam_filter_on field
-    if access_token.is_scam_filter_on:
-        access_token.is_scam_filter_on = False
+        # Toggle the is_scam_filter_on status
+        access_token.is_scam_filter_on = not access_token.is_scam_filter_on
+        access_token.save()
+
+        # Update the hidden field value (example: increment by 1)
+        new_hidden_field_value = int(hidden_field_value) + 1
+
+        return JsonResponse({
+            'success': True,
+            'is_scam_filter_on': access_token.is_scam_filter_on,
+            'newHiddenFieldValue': new_hidden_field_value
+        })
     else:
-        access_token.is_scam_filter_on = True
-    access_token.save()
+        return JsonResponse({'success': False})
 
-    return JsonResponse({'success': True, 'is_scam_filter_on': access_token.is_scam_filter_on})
 
 
 
